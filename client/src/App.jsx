e015fc99f354001f58c7d71d200ce4f3e46ed0e0
Ashish <ashishrajle700@gmail.com>
2023-10-24 13:27:39 +0530

    add react-router-dom and create Pages components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signout from "./pages/Signout";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 const App = () => {
   console.log("ashish");
-  return <div className="bg-slate-600">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<Signout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
