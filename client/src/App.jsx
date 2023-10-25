68b11397f5e159ea98ca2f1535e45a1b67630331
Ashish <ashishrajle700@gmail.com>
2023-10-25 13:14:57 +0530

    Correction route names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 //pages
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
-import Signout from "./pages/Signout";
+import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-out" element={<Signout />} />
+        <Route path="/sign-up" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
       </Routes>
