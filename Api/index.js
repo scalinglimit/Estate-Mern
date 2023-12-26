2fc2faa2fcb5693cefc20c34f42b0a20e9331ec7
Ashish <ashishrajle700@gmail.com>
2023-12-26 16:02:35 +0530

    added mongoose model (user.model)

--- Api/index.js
+++ Api/index.js
@@ -1,17 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+
 import dotenv from "dotenv";
 dotenv.config();
+
+const app = express();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
+    app.listen(3000, () => console.log("server is running on port 3000"));
     console.log("connected to Mongodb!");
   })
   .catch((err) => {
     console.log(err);
   });
-
-const app = express();
-
-app.listen(3000, () => console.log("server is running on port 3000"));
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div className="bg-slate-950">
-      <h1 className="text-white">Hello Home</h1>
-      <div>
-        <h1 className=""></h1>
-      </div>
-    </div>
-  );
+  return <div> Home</div>;
 };
 
 export default Home;
