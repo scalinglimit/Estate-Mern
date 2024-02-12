f6461aa4bc07d57915569e0a6ecb54354f28ca97
Ashish <ashishrajle700@gmail.com>
2024-02-12 18:35:39 +0530

    create a middleware and function(utils folder error.js) to handle the errors

--- Api/index.js
+++ Api/index.js
@@ -26,3 +26,14 @@ mongoose
 app.use("/api/user", userRoutes);
 
 app.use("/api/auth", authRouter);
+
+//error middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: "false",
+    statusCode,
+    message,
+  });
+});
